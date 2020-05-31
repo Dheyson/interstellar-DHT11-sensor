5085d49474a1f6e36bf032bd009127d4b29cca6f
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-05-30 23:52:44 -0300

    feat: Create Axios routes in the api file

---
+++ frontend/web/src/api.js
@@ -0,0 +1,54 @@
+const axios = require('axios').default;
+
+const instance = axios.create({
+	baseURL: 'https://api.thingspeak.com/',
+	timeout: 1000,
+	headers: { 'X-Custom-Header': 'foobar' }
+});
+
+function getPublicChannels() {
+	return instance.get(`users/${process.env.USER_ID}/channels.json`)
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+}
+
+function getFieldById(fieldId) {
+	return instance.get(`channels/${process.env.CHANNEL_ID}/${fieldId}.json`)
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+}
+
+function getCurrentStatus() {
+	return instance.get(`channels/${process.env.CHANNEL_ID}/status.json`)
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+}
+
+function getData() {
+	return instance.get(`channels/${process.env.CHANNEL_ID}/feeds.json`)
+		.then(function (response) {
+			console.log(response);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
+}
+
+axios.all([getPublicChannels(), getFieldById(), getCurrentStatus(), getData()])
+	.then(axios.spread(function (acct, perms) {
+		// Both requests are now complete
+	}));
+
+module.exports = [getFieldById, getPublicChannels, getCurrentStatus, getData];
