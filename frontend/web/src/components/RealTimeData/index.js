c42a85058fd41d03f132e131dcf7dfa5e52ac873
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-06-03 18:25:44 -0300

    feat: Changing the realtime component

--- frontend/web/src/components/RealTimeData/index.js
+++ frontend/web/src/components/RealTimeData/index.js
@@ -1,46 +1,83 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
 import * as S from './styles';
 
 import SmallData from './SmallData';
 
-import { getCurrentStatus, getData, getFieldById, getPublicChannels } from '../../api';
-
-const RealTimeData = () => {
-	const [data, setData] = useState([]);
-
-	useEffect(() => {
-		async function fetchData() {
-			await axios.get(`https://api.thingspeak.com/channels/1064896/fields/1.json`)
-				.then(res => {
-					const values = res.data.feeds;
-					setData({ values });
-				})
+export default class RealTimeData extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			channels: {},
+			field1: '',
+			field2: '',
+			field3: '',
+			field4: ''
 		}
-		fetchData();
-	}, []);
-
-	function handleData() {
-		console.log(data);
 	}
 
-	return (
-		<S.Container>
-			<S.Title>
-				Real time data
+	async componentDidMount() {
+		await axios.get(`https://api.thingspeak.com/channels/1064896/fields/1.json`)
+			.then(res => {
+				this.setState({ channels: res.data.channel, feeds: res.data.feeds[0] })
+			})
+			.catch(err => {
+				console.log(err);
+			})
+
+		await axios.get(`https://api.thingspeak.com/channels/1064896/fields/1/last.json`)
+			.then(res => {
+				this.setState({ field1: res.data.field1 })
+			})
+			.catch(err => {
+				console.log(err);
+			})
+
+		await axios.get(`https://api.thingspeak.com/channels/1064896/fields/2/last.json`)
+			.then(res => {
+				this.setState({ field2: res.data.field2 })
+			})
+			.catch(err => {
+			})
+
+		await axios.get(`https://api.thingspeak.com/channels/1064896/fields/3/last.json`)
+			.then(res => {
+				this.setState({ field3: res.data.field3 })
+
+			})
+			.catch(err => {
+				console.log(err);
+			})
+
+		await axios.get(`https://api.thingspeak.com/channels/1064896/fields/4/last.json`)
+			.then(res => {
+				this.setState({ field4: res.data.field4 })
+			})
+			.catch(err => {
+				console.log(err);
+			})
+	}
+
+	render() {
+		const { channels, field1, field2, field3, field4 } = this.state;
+
+		return (
+			<S.Container>
+				<S.Title>
+					Real time data
 			</S.Title>
-			<S.DataContainer>
-				<SmallData fieldText="" value="" />
-				<SmallData fieldText="test1" value="23" />
-			</S.DataContainer>
-			<S.DataContainer>
-				<SmallData fieldText="test1" value="23" />
-				<SmallData fieldText="test1" value="23" />
-			</S.DataContainer>
-			<button onClick={handleData}>ok</button>
-		</S.Container>
-	);
+				<S.DataContainer>
+					<SmallData fieldText={channels.field1} value={field1 || 'IsEmpty'} />
+					<SmallData fieldText={channels.field2} value={field2 || 'IsEmpty'} />
+				</S.DataContainer>
+				<S.DataContainer>
+					<SmallData fieldText={channels.field3} value={field3 || 'IsEmpty'} />
+					<SmallData fieldText={channels.field4} value={field4 || 'IsEmpty'} />
+				</S.DataContainer>
+			</S.Container>
+		);
+	}
+
 }
 
-export default RealTimeData;
