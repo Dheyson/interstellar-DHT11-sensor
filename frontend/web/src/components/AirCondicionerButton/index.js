e7bd5586d70b971c8c6dfc55d270ed5eb360af2b
Dheyson Alves <dheyson.lucas@hotmail.com>
2020-06-03 21:13:54 -0300

    feat: Update the iframes

--- frontend/web/src/components/AirCondicionerButton/index.js
+++ frontend/web/src/components/AirCondicionerButton/index.js
@@ -13,9 +13,9 @@ const AirCondicionerButton = () => {
 	return (
 		<div className="container">
 			<div className="switch">
-				<input type="radio" className="switch-input" name="view" value="on" id="on" checked />
+				<input type="radio" className="switch-input" name="view" value="on" id="on"  />
 				<label for="on" className="switch-label switch-label-off">ON</label>
-				<input type="radio" className="switch-input" name="view" value="off" id="off" />
+				<input type="radio" className="switch-input" name="view" value="off" id="off" checked/>
 				<label for="off" className="switch-label switch-label-on">OFF</label>
 				<span className="switch-selection"></span>
 			</div>
--- frontend/web/src/components/LogData/index.js
+++ frontend/web/src/components/LogData/index.js
@@ -10,7 +10,7 @@ const RealTimeData = () => {
 				Log data
 			</S.Title>
 			<S.TablesContainer>
-				<S.Iframe src="https://thingspeak.com/channels/1064896/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature%28C%29&type=column"></S.Iframe>
+				<S.Iframe src="https://thingspeak.com/channels/1064896/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature+%28C%29&type=line&yaxismax=40&yaxismin=0"></S.Iframe>
 				<S.Iframe src="https://thingspeak.com/channels/1064896/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Temperature%28F%29&type=line"></S.Iframe>
 			</S.TablesContainer>
 			<S.TablesContainer>
